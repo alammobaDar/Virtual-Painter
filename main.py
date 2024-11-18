ad6ff662b94a82c2f71dd46ed24aa7eeca5622ec
Darren <edrichdarrensantuyo172030@gmail.com>
2024-11-18 22:43:44 +0800

    set the image canvas to just flip and not show

--- main.py
+++ main.py
@@ -4,18 +4,13 @@ import numpy as np
 import time
 
 
-
 def fingers_is_close(index_x, index_y, middle_x, middle_y):
-      if (index_x + index_y) - (middle_x + middle_y) <=0.06:
-        print(f"index({index_x + index_y})")
-        print(f"middle({middle_x + middle_y})")
+      if (index_x + index_y) - (middle_x + middle_y) <= 0.065:
         return True
-      
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
-
 
 img_canvas = np.zeros((480, 640, 3), np.uint8)
 
@@ -40,6 +35,14 @@ with mp_hands.Hands(
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+
+    img_gray = cv2.cvtColor(img_canvas, cv2.COLOR_BGRA2GRAY)
+    _, img_inv = cv2.threshold(img_gray, 50, 255, cv2.THRESH_BINARY_INV)
+    img_inv = cv2.cvtColor(img_inv, cv2.COLOR_GRAY2BGR)
+    image = cv2.bitwise_and(image, img_inv)
+    image = cv2.bitwise_or(image, img_canvas)
+
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
           mp_drawing.draw_landmarks(
@@ -72,8 +75,11 @@ with mp_hands.Hands(
               cv2.line(image, (xp,yp),(cartesian_x, cartesian_y), (0,0,0), 10)
               cv2.line(img_canvas, (xp,yp),(cartesian_x, cartesian_y), (0, 255,0), 20)
               xp, yp = cartesian_x, cartesian_y
+
+
           
-    cv2.imshow("Canvas", cv2.flip(img_canvas, 1))
+          
+    cv2.flip(img_canvas, 1)
     # Flip the image horizontally for a selfie-view display.
     cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
 
