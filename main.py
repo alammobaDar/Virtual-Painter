feae20b613991f6afafd4aa1a293bb9f6fa3e875
Darren <edrichdarrensantuyo172030@gmail.com>
2024-11-21 12:20:55 +0800

    optimized the coordinates

--- main.py
+++ main.py
@@ -6,16 +6,27 @@ import time
 
 HEIGHT = 720
 WIDTH = 1280
+DISTANCE = 0.065
+PEN_COLOR = (0, 255, 0)
+PEN_SIZE = 20
 
-def fingers_is_close(index_x, index_y, middle_x, middle_y):
-      if (index_x + index_y) - (middle_x + middle_y) <= 0.065:
-        return True
+
+def fingers_is_close(index_x, index_y, middle_x, middle_y, index_z):
+      if (index_x + index_y) - (middle_x + middle_y) / index_z >= 2:
+        # print(f"depth({(index_x + index_y) - (middle_x + middle_y) / index_z})")
+        if (index_x + index_y) - (middle_x + middle_y) <= DISTANCE:
+          # print(f"z({index_z})")
+          # print(f"difference({(index_x + index_y) - (middle_x + middle_y)})")
+          return True
+
+
+def circle_between(image, x, y):
+    cv2.circle(image, (x, y), 20, (0,255,0), -1)
+
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
-
-
 
 img_canvas = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
 
@@ -67,24 +78,34 @@ with mp_hands.Hands(
 
           index_num_x = float(index_tip_landmark.x)
           index_num_y = float(index_tip_landmark.y)
-
+          index_num_z = float(index_tip_landmark.z)
           middle_num_x = float(middle_tip_landmark.x)
           middle_num_y = float(middle_tip_landmark.y)
         # for the tip of the pen
-          
-          cartesian_x = int((index_num_x)* cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-          cartesian_y = int((index_num_y)* cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+          cartesian_index_x = int((index_num_x)* cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+          cartesian_index_y = int((index_num_y)* cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+          cartesian_middle_x= int((middle_num_x)* cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+          cartesian_middle_y = int((middle_num_y)* cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-          if fingers_is_close(index_x=index_num_x, index_y=index_num_y, middle_x=middle_num_x, middle_y=middle_num_y):
-            cv2.circle(image, (cartesian_x, cartesian_y), 10, (0,255,0), -1)
+          #combination of xy of middle and index
+          combination_x =  int((cartesian_index_x + cartesian_middle_x)/2)
+          combination_y = int((cartesian_index_y + cartesian_middle_y)/2)
+          
+          circle_between(image, combination_x, combination_y)
+
+          if fingers_is_close(index_x=index_num_x, index_y=index_num_y, middle_x=middle_num_x, middle_y=middle_num_y, index_z=index_num_z):
+            # cv2.circle(image, (combination_x, combination_y), 10, (0,255,0), -1)
+            print(f"combination x({combination_x})")
+            print(f"combination y({combination_y})")
+            
             if True:
               xp, yp = 0, 0
 
               if xp == 0 and yp == 0:
-                xp, yp = cartesian_x, cartesian_y
-              cv2.line(image, (xp,yp),(cartesian_x, cartesian_y), (0,0,0), 10)
-              cv2.line(img_canvas, (xp,yp),(cartesian_x, cartesian_y), (0, 255,0), 20)
-              xp, yp = cartesian_x, cartesian_y
+                xp, yp = cartesian_index_x, cartesian_index_y
+              cv2.line(image, (xp,yp),(cartesian_index_x, cartesian_index_y), PEN_COLOR, PEN_SIZE)
+              cv2.line(img_canvas, (xp,yp),(cartesian_index_x, cartesian_index_y), PEN_COLOR, PEN_SIZE)
+              xp, yp = cartesian_index_x, cartesian_index_y
 
 
           
