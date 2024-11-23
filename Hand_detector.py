fdbf6a81bd831807567336e5821be82bf66a513f
Darren <edrichdarrensantuyo172030@gmail.com>
2024-11-23 20:35:38 +0800

    able to detect hands now

--- Hand_detector.py
+++ Hand_detector.py
@@ -14,3 +14,32 @@ class Hand_detection:
         self.mp_hands = mp.solutions.hands
         self.hands = self.mp_hands.Hands(self.mode, self.max_hands, self.complexity, self.detect, self.tracking)
         self.mp_draw = mp.solutions.drawing_utils
+
+    def find_hands(self, img):
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(img_rgb)
+
+        if self.results.multi_hand_landmarks:
+            for hand_landmarks in self.results.multi_hand_landmarks:
+                self.mp_draw.draw_landmarks(img, hand_landmarks,self.mp_hands.HAND_CONNECTIONS)
+
+def main():
+
+    cap = cv2.VideoCapture(0)
+    detector = Hand_detection()
+
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("nigger")
+            continue
+
+        detector.find_hands(image)
+
+        cv2.imshow('Hand Detection', cv2.flip(image, 1))
+
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+
+if __name__ == "__main__":
+    main()
